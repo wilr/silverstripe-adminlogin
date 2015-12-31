5d15696aed30575423cb492edeed44e0782aa5ce
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:49:29 +1300

    Converted to PSR-2

--- code/LimitAdminAccessExtension.php
+++ code/LimitAdminAccessExtension.php
@@ -1,20 +1,22 @@
 <?php
 
-class LimitAdminAccessExtension extends Extension {
-	
-	function onBeforeInit() {
-		if(Config::inst()->get('IpAccess', 'enabled')) {
-			$ipAccess = new IpAccess($this->owner->getRequest()->getIP(), Config::inst()->get('IpAccess', 'allowed_ips'));
-				
-			if(!$ipAccess->hasAccess()) {
-				if(class_exists('ErrorPage', true)) {
-					$response = ErrorPage::response_for(403);
-				}
+class LimitAdminAccessExtension extends Extension
+{
+    
+    public function onBeforeInit()
+    {
+        if (Config::inst()->get('IpAccess', 'enabled')) {
+            $ipAccess = new IpAccess($this->owner->getRequest()->getIP(), Config::inst()->get('IpAccess', 'allowed_ips'));
+                
+            if (!$ipAccess->hasAccess()) {
+                if (class_exists('ErrorPage', true)) {
+                    $response = ErrorPage::response_for(403);
+                }
 
-				$response = ($response) ? $response : 'The requested page could not be found.';
+                $response = ($response) ? $response : 'The requested page could not be found.';
 
-				return $this->owner->httpError(403, $response);
-			}
-		}
-	}
+                return $this->owner->httpError(403, $response);
+            }
+        }
+    }
 }
